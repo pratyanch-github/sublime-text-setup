25abc3b49f4b12caa7ce41fcb72484b286844efa
Pratyanch dongre <69388801+pratyanch-github@users.noreply.github.com>
2023-01-29 05:35:58 +0530

    Create insta.cpp

---
+++ insta.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+   
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+  int t=1;
+  cin>>t;
+  for(int i=0;i<t;i++){
+    solve();
+  }
+}

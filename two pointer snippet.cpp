11542ac695d578579ec910ac40569548bd178628
Pratyanch dongre <69388801+pratyanch-github@users.noreply.github.com>
2023-01-29 05:45:17 +0530

    Create two pointer snippet.cpp

---
+++ two pointer snippet.cpp
@@ -0,0 +1,24 @@
+
+long long head=-1
+long long tail=0;
+long long ans;
+
+while(tail<n)
+{
+    while(head+1<n and  )
+    {
+        head++;
+    }
+
+    // process ans
+
+    if(head>=tail)
+    {   
+        //process dependencies
+        tail++;
+    }
+    else {
+        tail++;
+        head=tail-1;
+    }
+}
